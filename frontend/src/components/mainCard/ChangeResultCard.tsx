468544375da1ef7d156079e90722551501d95da7
bui <39144242+tbui1996@users.noreply.github.com>
2026-02-20 21:55:01 -0500

    udpat eiport

--- frontend/src/components/mainCard/ChangeResultCard.tsx
+++ frontend/src/components/mainCard/ChangeResultCard.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid, Box } from '@mui/material';
-import { ChangeResponse } from '../../api/types';
-
+import { ChangeResponse } from '../../types';
 interface ChangeResultCardProps {
   result: ChangeResponse;
 }
